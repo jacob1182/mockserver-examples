57245328997b18ed5b032cb3af5f2fbc0b0869b6
Jose A <jacob1182@gmail.com>
2020-12-20 18:51:30 +0100

    Create project structure

---
+++ src/main/kotlin/org/jasi/mockserver/examples/MockserverExamplesApplication.kt
@@ -0,0 +1,11 @@
+package org.jasi.mockserver.examples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MockserverExamplesApplication
+
+fun main(args: Array<String>) {
+    runApplication<MockserverExamplesApplication>(*args)
+}
